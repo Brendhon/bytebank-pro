ec104010900d9653d1eef0b346d942dde3060b47
Brendhon <brendhon.e.c.m@gmail.com>
2025-06-19 20:58:35 -0300

    refactor: remove module federation config files and clean up webpack configuration

--- apps/dashboard/webpack.config.js
+++ apps/dashboard/webpack.config.js
@@ -1,15 +1,12 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
   name: 'dashboard',
-
+  filename: 'remoteEntry.js',
   exposes: {
     './Component': './src/app/app.ts',
   },
-
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
-
+  }
 });
--- apps/shell/webpack.config.js
+++ apps/shell/webpack.config.js
@@ -1,13 +1,14 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
+  name: 'shell',
+  exposes: {},
   remotes: {
     "dashboard": "http://localhost:4201/remoteEntry.js",
     "transactions": "http://localhost:4202/remoteEntry.js",
     "settings": "http://localhost:4203/remoteEntry.js"
   },
-
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  }
 });
