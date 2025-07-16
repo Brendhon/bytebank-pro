98f43f2caa8a0ab3501e4ef48d9c4c5c0cc6519a
Brendhon <brendhon.e.c.m@gmail.com>
2025-07-15 21:17:20 -0300

    fix: update baseUrl for development and production environments in dashboard and transactions applications

--- apps/dashboard/src/environments/environment.development.ts
+++ apps/dashboard/src/environments/environment.development.ts
@@ -3,5 +3,5 @@ import { IEnvironment } from '@bytebank-pro/types';
 export const environment: IEnvironment = {
   production: false,
   apiUrl: 'http://localhost:3000/graphql',
-  baseUrl: 'https://bytebank-pro-dashboard.vercel.app' // URL base do MFE dashboard
+  baseUrl: 'http://localhost:4201' // URL base do MFE dashboard
 };
--- apps/dashboard/src/environments/environment.ts
+++ apps/dashboard/src/environments/environment.ts
@@ -3,5 +3,5 @@ import { IEnvironment } from '@bytebank-pro/types';
 export const environment: IEnvironment = {
   production: true,
   apiUrl: 'https://bytebank-api.onrender.com/graphql',
-  baseUrl: 'http://localhost:4201' // URL base do MFE dashboard
+  baseUrl: 'https://bytebank-pro-dashboard.vercel.app' // URL base do MFE dashboard
 };
